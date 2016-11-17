30389e85d14b96618df3f264ee701b913e6b8391
vinaygulati <vgulati28@gmail.com>
2016-11-18 03:42:12 +0530

    #ng repeat added in home page

--- src/main/webapp/app/home/home.state.js
+++ src/main/webapp/app/home/home.state.js
@@ -21,8 +21,30 @@
                     controllerAs: 'vm'
                 }
             },
+            params: {
+                page: {
+                    value: '1',
+                    squash: true
+                },
+                sort: {
+                    value: 'id,asc',
+                    squash: true
+                },
+                search: null
+            },
             resolve: {
+            	pagingParams: ['$stateParams', 'PaginationUtil', function ($stateParams, PaginationUtil) {
+                    return {
+                        page: PaginationUtil.parsePage($stateParams.page),
+                        sort: $stateParams.sort,
+                        predicate: PaginationUtil.parsePredicate($stateParams.sort),
+                        ascending: PaginationUtil.parseAscending($stateParams.sort),
+                        search: $stateParams.search
+                    };
+                }],
                 mainTranslatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate,$translatePartialLoader) {
+                	$translatePartialLoader.addPart('blog');
+                    $translatePartialLoader.addPart('global');
                     $translatePartialLoader.addPart('home');
                     return $translate.refresh();
                 }]
