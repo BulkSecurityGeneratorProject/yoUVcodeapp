6544aa55ec43d9ce2d577f2d9bb6f8ed1e90ecda
vinaygulati <vgulati28@gmail.com>
2016-11-18 03:39:40 +0530

    #blog view count functionality added

--- src/main/webapp/app/entities/blog/blog-detail.controller.js
+++ src/main/webapp/app/entities/blog/blog-detail.controller.js
@@ -13,6 +13,28 @@
         vm.blog = entity;
         vm.byteSize = DataUtils.byteSize;
         vm.openFile = DataUtils.openFile;
+        vm.blog.hits = ++vm.blog.hits;
+        
+        vm.save = save;
+        save();
+        
+        function save () {
+            vm.isSaving = true;
+            if (vm.blog.id !== null) {
+                Blog.update(vm.blog, onSaveSuccess, onSaveError);
+            } else {
+                Blog.save(vm.blog, onSaveSuccess, onSaveError);
+            }
+        }
+
+        function onSaveSuccess (result) {
+            $scope.$emit('yoUVcodeApp:blogUpdate', result);
+            vm.isSaving = false;
+        }
+
+        function onSaveError () {
+            vm.isSaving = false;
+        }
 
         var unsubscribe = $rootScope.$on('yoUVcodeApp:blogUpdate', function(event, result) {
             vm.blog = result;
