8200da6eab7a6eb2f3852347ddda879e3732b59e
umangparekh001 <umang.m.parekh@gmail.com>
2016-12-10 21:21:17 +0530

    #Blog-Directive-Dashboard

---
+++ src/main/webapp/app/components/blog/blog-info.directive.js
@@ -0,0 +1,24 @@
+(function() {
+    'use strict';
+
+    var blogInfo = {
+        templateUrl: '/app/components/blog/blog-info.html',
+        bindings: {
+			blog : '<'
+		},
+        controller: blogInfoController
+    };
+
+    angular
+        .module('yoUVcodeApp')
+        .component('blogInfo', blogInfo);
+   
+    blogInfoController.$inject = ['$scope', 'AlertService'];
+
+    function blogInfoController($scope) {
+        var ctrl = this;
+
+        $scope.$on('$destroy', function () {
+        });
+    }
+})();
